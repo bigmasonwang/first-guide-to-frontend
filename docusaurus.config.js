e14fbf8be0cbd5173e5d6d3d8474012653c658d8
Mason Wang <bigmasonwang@gmail.com>
2022-06-21 22:23:13 +0930

    title and desc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Fist Guid To Frontend',
+  tagline: 'cool',
   url: 'https://github.com.bigmasonwang',
   baseUrl: '/first-guide-to-frontend/',
   onBrokenLinks: 'throw',
